91b90a3947e114352de40e632afca064fdaeb860
SasaGomes <sabrina.gomess.dev@gmail.com>
2026-01-29 22:06:14 -0300

    feat: busca usuário por id, deleta e atualiza

--- modules/express.js
+++ modules/express.js
@@ -5,17 +5,27 @@ const app = express();
 
 app.use(express.json());
 
+
 app.get("/users", async (req, res) => {
     try{
         //pega todos os usuários
         const users = await UserModel.find({});
-
         res.status(200).json(users);
 
     }catch(error){
         res.status(500).send(error);
     }
 });
+
+app.get("/users/:id", async (req, res) => {
+    try{
+        const id = req.params.id;
+        const user = await UserModel.findById(id);
+        res.status(200).json(user);
+    }catch (error){
+        res.status(500).send(error.message);
+    }
+})
 
 app.post('/users', async (req, res) => {
     try{
@@ -25,7 +35,30 @@ app.post('/users', async (req, res) => {
     }catch (error){
         res.status(500).send(error.message);
     }  
+});
+
+
+app.delete("/users/:id", async (req, res) =>{
+    try{
+        const id = req.params.id;
+        const user = await UserModel.findByIdAndDelete(id);
+        res.status(204).json(user);
+    } catch(error){
+        res.status(500).send(error);
+    }
 })
+
+app.patch("/users/:id", async (req, res) => {
+    try{
+        const id = req.params.id;
+        //O new pega o usuário atualizado do banco
+        const user = await UserModel.findByIdAndUpdate(id, req.body, { new: true});
+        res.status(200).json(user);
+    }catch (error){
+        res.status(500).send(error.message)
+    }
+})
+
 
 const port = 3000;
 
